af1bdc5231ed911f0c4b01bf9e202170060133ea
Dung Tri Nguyen <nguyentridung165@gmail.com>
2023-10-23 21:43:35 +0700

    - Add Drawer and empty features fragment - Implement basic Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.dungnguyen.pokeworld"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.dungnguyen.pokeworld"
@@ -33,15 +33,38 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    // Features
+    implementation(project(":features:pokedex"))
+    implementation(project(":features:generations"))
+    implementation(project(":features:moves"))
+    implementation(project(":features:machines"))
+    implementation(project(":features:items"))
+    implementation(project(":features:berries"))
+    implementation(project(":features:locations"))
+
+    // Core
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+
+    // Constraintlayout
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+
 }
--- app/src/main/java/com/dungnguyen/pokeworld/MainActivity.kt
+++ app/src/main/java/com/dungnguyen/pokeworld/MainActivity.kt
@@ -2,10 +2,117 @@ package com.dungnguyen.pokeworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.MenuItem
+import androidx.annotation.IdRes
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.dungnguyen.pokeworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
+    private val navController by lazy {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.navController
+    }
+
+    private val appBarConfiguration by lazy {
+        AppBarConfiguration(
+            topLevelDestinationIds = setOf(
+                R.id.pokedex_fragment,
+                R.id.generations_fragment,
+                R.id.moves_fragment,
+                R.id.machines_fragment,
+                R.id.items_fragment,
+                R.id.berries_fragment,
+                R.id.locations_fragment,
+            )
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupActionBar()
+        setupNavigationDrawer()
+        navigateToFragment(R.id.pokedex_fragment)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbar)
+//        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        actionBarDrawerToggle = ActionBarDrawerToggle(
+            this, binding.drawerLayout, R.string.nav_open, R.string.nav_close
+        )
+
+        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    private fun setupNavigationDrawer() {
+        binding.navigationView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_pokedex -> {
+                    navigateToFragment(R.id.pokedex_fragment)
+                }
+
+                R.id.nav_generations -> {
+                    navigateToFragment(R.id.generations_fragment)
+                }
+
+                R.id.nav_moves -> {
+                    navigateToFragment(R.id.moves_fragment)
+                }
+
+                R.id.nav_machines -> {
+                    navigateToFragment(R.id.machines_fragment)
+                }
+
+                R.id.nav_items -> {
+                    navigateToFragment(R.id.items_fragment)
+                }
+
+                R.id.nav_berries -> {
+                    navigateToFragment(R.id.berries_fragment)
+                }
+
+                R.id.nav_locations -> {
+                    navigateToFragment(R.id.locations_fragment)
+                }
+            }
+            closeDrawers()
+            return@setNavigationItemSelectedListener true
+        }
+    }
+
+    private fun navigateToFragment(@IdRes id: Int) {
+        navController.navigate(id)
+    }
+
+    private fun closeDrawers() {
+        binding.drawerLayout.closeDrawers()
     }
 }
