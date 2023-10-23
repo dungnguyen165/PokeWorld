f620817b9ed2b5c7b128c45506e3148af8129086
Dung Tri Nguyen <nguyentridung165@gmail.com>
2023-10-23 19:27:41 +0700

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PokeWorld"
+include(":app")
+include(":core")
+include(":features:pokedex")
+include(":features:generations")
+include(":features:items")
+include(":features:machines")
+include(":features:locations")
+include(":features:moves")
+include(":features:berries")
+include(":logging")
+include(":analytics")
